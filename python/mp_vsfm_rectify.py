7050e11b4a4f1d3b354877590d032bfa0c1800c8
Robert Twomey <rtwomey@uw.edu>
2015-04-25 11:08:16 -0700

    can reorder images in mp_vsfm_rectify

--- python/mp_vsfm_rectify.py
+++ python/mp_vsfm_rectify.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 """
     multi-threaded program to apply homographies 
     calculated with Visual SFM on windows to 
@@ -31,7 +31,7 @@ from pylab import *
 def readImageFilenames(dir_name):
     # read in image filenames
     dir_list = []
-    for extension in (".png", ".jpg", ".jpeg"):
+    for extension in (".png", ".jpg", ".jpeg", '.JPG'):
         try:
             temp = os.listdir(dir_name)
             temp = filter(lambda x: x.find(extension) > -1, temp)
@@ -149,6 +149,28 @@ def readCamerasV2File(camerasfile):
     
     return camdict, np.array(avg_R).reshape((3,1)), (avgxcenter, avgycenter)
 
+def write_blank(src):
+    global warpedpath, undistortpath, thumbpath, acq_grid, max_texture_size, num_textures
+    
+    src_img_rgb = cv2.imread(src)
+    
+    img_w = src_img_rgb.shape[1]
+    img_h = src_img_rgb.shape[0]
+    
+    src_img_warp = np.zeros((img_h, img_w, 3))
+    fname, ext = os.path.splitext(os.path.basename(src))
+    warp_file = os.path.join(warpedpath, fname+"_warp"+ext.lower())
+    cv2.imwrite(warp_file, src_img_warp)
+    
+    # generate thumbnail
+    acq_w, acq_h = acq_grid
+    thumb_w = max_texture_size / acq_w * num_textures
+    thumb_h = thumb_w * img_h / img_w
+    
+    thumb_img = np.zeros((thumb_h, thumb_w, 3))
+    thumb_file = os.path.join(thumbpath, fname+ext.lower())
+    cv2.imwrite(thumb_file, thumb_img)
+    
     
 def mp_warp(srcs, params, R_avg, C_avg, nprocs):
     
@@ -164,8 +186,11 @@ def mp_warp(srcs, params, R_avg, C_avg, nprocs):
 
         for src in srcs:
             src_fname = os.path.basename(src)
-            outdict[src] = warpAvg(src, params[src_fname], R_avg, C_avg)
-            print ".",
+            if src_fname in params.keys():
+                outdict[src] = warpAvg(src, params[src_fname], R_avg, C_avg)
+            else:
+                write_blank(src)
+            sys.stdout.write(".")
             sys.stdout.flush()
             
         out_q.put(outdict)
@@ -199,21 +224,30 @@ def mp_warp(srcs, params, R_avg, C_avg, nprocs):
             
             
 def warpAvg(src, params, R_avg, C_avg):
-    global warpedpath, undistortpath, thumbpath, acq_grid, max_texture_size
+    global warpedpath, undistortpath, thumbpath, acq_grid, max_texture_size, num_textures
     
     # params for src image
     f1, T1, C1, R_mat1, R_quat1, normRadialDistortion1, undistort_fname = params
 
     # image files
+    
     undistort = os.path.join(undistortpath, undistort_fname)
     src_img_rgb = cv2.imread(undistort)
 
+    # name, ext = os.path.splitext(os.path.basename(src))
+    # eq_img = os.path.join(undistortpath, name + ".jpg")#"_eq.jpg")
+
+    #print eq_img
+
+    # src_img_rgb = cv2.imread(eq_img)
+
+    #src_img_rgb = cv2.imread(src)
+    
     w = src_img_rgb.shape[1]
     h = src_img_rgb.shape[0]
 
-    # magic number
-    w_offset = (C1[0] - C_avg[0]) * 200#w/2
-    h_offset = (C1[2] - C_avg[1]) * 200# w/2
+    w_translate = (C1[0] - C_avg[0]) #* f1 #* 200#w/2
+    h_translate = (C1[2] - C_avg[1]) #* f1 #* 200# w/2
 
        # http://math.stackexchange.com/questions/87338/change-in-rotation-matrix
 
@@ -233,12 +267,14 @@ def warpAvg(src, params, R_avg, C_avg):
     # R_rod2, jacobian = cv2.Rodrigues(R_mat2)
     # print "R_rod2:", R_rod2
 
-    R_rod_12 = R_rod1 - R_avg #R_avg - R_rod1 #
+    R_rod_12 = R_rod1 - R_avg
+    # R_rod_12 = R_avg - R_rod1
+    
     #print "R_rod_12:", R_rod_12 * (180 / pi)
     # print R_rod_12
 
     # switch order of 2nd and 3rd rotation
-    R_rod_12 = np.array([R_rod_12[0], R_rod_12[1], -1*R_rod_12[2]])
+    R_rod_12 = np.array([R_rod_12[0], R_rod_12[1], -1 * R_rod_12[2]])
     
     #lprint R_rod_12
     
@@ -291,12 +327,9 @@ def warpAvg(src, params, R_avg, C_avg):
     R = R_12 #R_mat1.T
 
     #Create trans mat and combine with translation matrix
-    T = np.array([[1], [0], [-w/2+w_offset],
-                       [0], [1], [-h/2+h_offset],
+    T = np.array([[1], [0], [(-w/2)+w_translate],
+                       [0], [1], [(-h/2)+h_translate],
                        [0], [0], [1]]).reshape((3,3))
-    # T[2,2] = 2
-    # T = T / T[2,2]
-
     #print "T:", T
 
     R[0,2] = 0
@@ -307,7 +340,7 @@ def warpAvg(src, params, R_avg, C_avg):
     trans[2,2] += w
 
     # adjustment to focal length
-    fscale = 0.8
+    fscale = 1.0
     K = np.array([[f1*fscale], [0], [w/2],
                   [0], [f1*fscale], [h/2],
                   [0], [0], [1]]).reshape(3,3)
@@ -317,7 +350,7 @@ def warpAvg(src, params, R_avg, C_avg):
     H = np.dot(K, trans)
 
     # pad and recenter
-    oversize = 0.05
+    oversize = 0#0.05
     
     move_h = np.matrix(np.identity(3), np.float32)
 
@@ -335,27 +368,84 @@ def warpAvg(src, params, R_avg, C_avg):
     src_img_warp = cv2.warpPerspective(src_img_rgb, H, (img_w, img_h))#w, h))
 
     fname, ext = os.path.splitext(os.path.basename(src))
-    warp_file = os.path.join(warpedpath, fname+"_warp"+ext)
+    warp_file = os.path.join(warpedpath, fname+"_warp"+ext.lower())
     cv2.imwrite(warp_file, src_img_warp)
     
     # generate thumbnail
     acq_w, acq_h = acq_grid
-    thumb_w = max_texture_size / acq_w
+    thumb_w = max_texture_size / acq_w * num_textures
     thumb_h = thumb_w * img_h / img_w
     
     thumb_img = cv2.resize(src_img_warp, (thumb_w, thumb_h))
-    thumb_file = os.path.join(thumbpath, fname+ext)
+    thumb_file = os.path.join(thumbpath, fname+ext.lower())
     cv2.imwrite(thumb_file, thumb_img)
 
     # return (warp_file, thumb_file)
                
-                         
+
+def generate_contact_sheet(reorder = None):
+    global thumbpath, max_texture_size, grid_h, grid_w, num_textures, contactimg_file
+    
+    # generate tiled image
+    thumbstr = "image{0:04d}.jpg"
+    thumb_file = os.path.join(thumbpath, thumbstr.format(1))
+    thumb_img = cv2.imread(thumb_file)
+    thumb_h, thumb_w, thumb_chan = thumb_img.shape
+
+    max_texture_w = max_texture_size
+    max_texture_h = thumb_h * grid_h
+
+    print "generating contact sheet...",
+
+    part_w = grid_w/num_textures
+
+    for n in range(num_textures):
+        
+        contact_img = np.zeros((max_texture_h, max_texture_w, 3), np.uint8)
+        print contact_img.shape
+        
+        for j in range(grid_h):
+          for i in range(part_w):
+              num = (n * part_w) + i + (j * grid_w)
+              if reorder != None:
+                  #print num
+                  num = reorder[num]
+              thumb_file = os.path.join(thumbpath, thumbstr.format(num))
+              # print thumb_file,
+              if os.path.exists(thumb_file):
+                  thumb_img = cv2.imread(thumb_file)
+                  thumb_h, thumb_w, thumb_chan = thumb_img.shape
+                  x = i * thumb_w
+                  y = j * thumb_h
+                  # print x,y, thumb_w ,thumb_h
+                  # print contact_img.shape
+                  contact_img[y:(y+thumb_h), x:(x+thumb_w)] = thumb_img
+
+        print "writing contact image", contact_img.shape, "to",
+        fname, ext = os.path.splitext(contactimg_file)
+        contactimg_file = fname+str(n)+ext
+        print contactimg_file
+        cv2.imwrite(contactimg_file, contact_img)
+          
+                   
+def write_camera_positions(camerapos, image_files, camresults):
+    # save camera positions
+    count = 0
+    out_f = file(camerapos, 'w')
+    for src in image_files:
+        src_fname = os.path.basename(src)
+        if src_fname in camresults.keys():
+            C_src = camresults[src_fname][2]
+            out_f.write('{0} {1} {2}\n'.format(count, (C_avg[0]-C_src[0]), (C_avg[1]-C_src[2])))
+        count = count + 1
+
 if __name__ == '__main__':
-    global featurepath, warpedpath, undistortpath, thumbpath, max_texture_size, acq_grid
+    global featurepath, warpedpath, undistortpath, thumbpath, max_texture_size, acq_grid, num_textures, contactimg_file
 
     # Define command line argument interface
     parser = argparse.ArgumentParser(description='apply Visual SFM quaternions to a folder of lightfield images')
     parser.add_argument('inpath', help='path to input images')
+    parser.add_argument('grid', help='acquisition grid as WxH')
     parser.add_argument('undistortpath', help='path to undistorted images')
     parser.add_argument('camerasv2', help='cameras_v2.txt file from VSFM')
     parser.add_argument('warpedpath', help='output path to save the warped image files')
@@ -366,6 +456,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     imagedir = args.inpath  # directory of input images
+    gridstr = args.grid
     undistortpath = args.undistortpath # directory of undistorted images
     camerasfile = args.camerasv2  # results of N-view match from Visual SFM in windows
     warpedpath = args.warpedpath    # out path to save the results
@@ -373,65 +464,46 @@ if __name__ == '__main__':
     camerapos = args.camerapos
     contactimg_file = args.contactimg
     
-    # create output path if necessary
+    # create output paths as necessary
     if not os.path.exists(warpedpath):
         os.makedirs(warpedpath)
+
+    if not os.path.exists(thumbpath):
+        os.makedirs(thumbpath)
     
+    # grid
+    grid_w, grid_h = gridstr.split("x")
+    grid_w = int(grid_w)
+    grid_h = int(grid_h)
+    
+    print "grid dimensions", grid_w, "x", grid_h
+    
+    max_texture_size = 16384
+    num_textures = 1
+    acq_grid = (grid_w, grid_h)
+
     # read image filenames
     image_files = readImageFilenames(imagedir)
-    
+
     # read Cameras V2 file (results from VSFM)    
     camresults, R_avg, C_avg = readCamerasV2File(camerasfile)
     
-    # rectify image to average camera center and orientation from VSFM
-    # for src in image_files:
-    #     print "source:", src
-    #     src_fname = os.path.basename(src)
-    #     warpAvg(src, camresults[src_fname], R_avg, C_avg)
-
-    max_texture_size = 16384
-    grid_w = 29
-    grid_h = 20
-    acq_grid = (grid_w, grid_h)
-
+    # rectify images
     results = mp_warp(image_files, camresults, R_avg, C_avg, 8)
-    
-    
-    # generate tiled image
-    
-    thumb_file = os.path.join(thumbpath, "test_{0:04d}.jpg".format(0))
-    thumb_img = cv2.imread(thumb_file)
-    thumb_h, thumb_w, thumb_chan = thumb_img.shape
-    
-    max_texture_w = max_texture_size
-    max_texture_h = thumb_h * grid_h
-    
-    contact_img = np.zeros((max_texture_h, max_texture_w, 3), np.uint8)
-    
-    # print contact_img.shape
-    
-    for j in range(grid_h):
-        for i in range(grid_w):
-            num = i + (j * grid_w)
-            thumb_file = os.path.join(thumbpath, "test_{0:04d}.jpg".format(num))
-            # print thumb_file
-            if os.path.exists(thumb_file):
-                thumb_img = cv2.imread(thumb_file)
-                thumb_h, thumb_w, thumb_chan = thumb_img.shape
-                x = i * thumb_w
-                y = j * thumb_h
-                # print x,y, thumb_w ,thumb_h
-                contact_img[y:(y+thumb_h), x:(x+thumb_w)] = thumb_img
-            
-    print "writing contact image", contact_img.shape
-    cv2.imwrite(contactimg_file, contact_img)
-    
+        
+    # reorder
+    reorder = []
+    for i in range(grid_h):
+        if i%2==0:
+            reorder += range(i*grid_w, (i+1)*grid_w)
+        else:
+            reorder += range((i+1)*grid_w-1,i*grid_w-1, -1)
 
-    # save camera positions
-    count = 0
-    out_f = file(camerapos, 'w')
-    for src in image_files:
-        src_fname = os.path.basename(src)
-        C_src = camresults[src_fname][2]
-        out_f.write('{0} {1} {2}\n'.format(count, (C_avg[0]-C_src[0]), (C_avg[1]-C_src[2])))
-        count = count + 1
+    #print reorder
+    
+    # make contact sheet
+    generate_contact_sheet(reorder)
+
+    # export camera center coordinates    
+    write_camera_positions(camerapos, image_files, camresults)
+
