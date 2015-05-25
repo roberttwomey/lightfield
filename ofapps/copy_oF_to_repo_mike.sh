c9dd1385af96b4c9c708deddaefbece11dd98ddd
Robert Twomey <rtwomey@uw.edu>
2015-05-25 08:49:27 -0700

    updated tiled refocuser settings on ubuntu

--- ofapps/copy_oF_to_repo_mike.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-OF_ROVER_PATH=/Applications/of_v0.8.4_osx_release/apps/rover
-REPO_ROVER_PATH=/Users/admin/src/rover/ofapps
-rsync -avz --progress $OF_ROVER_PATH/* $REPO_ROVER_PATH
--- ofapps/copy_repo_to_oF_mike.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-OF_ROVER_PATH=/Applications/of_v0.8.4_osx_release/apps/rover
-REPO_ROVER_PATH=/Users/admin/src/rover/ofapps
-rsync -avz --progress $REPO_ROVER_PATH/* $OF_ROVER_PATH
