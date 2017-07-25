89458c8b25efee9e85f212b073fa8a3abb96d09d
Robert Twomey <rtwomey@ysu.edu>
2017-07-25 14:47:33 -0400

    kluged undistort for opencv 3.2, though it needs fixing

--- python/undistort.py
+++ python/undistort.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
+"""
+need to fix cv.Load methods below
+
+20170606
+
+"""
+
 import numpy as np
-import cv2, cv
+import cv2
 import os
 import multiprocessing
 from multiprocessing import Queue
@@ -77,8 +84,19 @@ if __name__ == '__main__':
     files = args.files  # path to input images
     
     # load camera matrix and distortion coefficients
-    camera_matrix = np.asarray(cv.Load(calib_filename, cv.CreateMemStorage(), 'camera_matrix'))
-    dist_coeffs = np.asarray(cv.Load(calib_filename, cv.CreateMemStorage(), 'distortion_coefficients'))
+    # fs = cv2.FileStorage(calib_filename, cv2.FILE_STORAGE_READ)
+    # # camera_matrix = np.asarray(fs.getNode('camera_matrix'))
+    # camera_matrix = np.asarray(fs['camera_matrix'])
+    # print camera_matrix
+    # exit()
+
+    # dist_coeffs = fs.getNode('distortion_coefficients')
+
+    # !!! TOTAL KLUGE, SHOULD READ FROM FILE !!!
+    camera_matrix = np.array([2.5148029100863805e+03, 0.0, 1.2932899929829127e+03, 0.0, 2.5196679186760493e+03, 9.2850768663425754e+02, 0.0, 0.0, 1.0]).reshape((3,3))
+    dist_coeffs = np.array([8.9556056301440312e-02, -2.4792541533716420e-01, -3.8170614382947539e-03, 6.4675682790495625e-04, -4.1359741393786997e-01])
+    # camera_matrix = np.asarray(cv2.Load(calib_filename, cv2.CreateMemStorage(), 'camera_matrix'))
+    # dist_coeffs = np.asarray(cv2.Load(calib_filename, cv2.CreateMemStorage(), 'distortion_coefficients'))
     
     print "Read camera calibration"
     print camera_matrix
--- python/vsfm_batch.sh
+++ python/vsfm_batch.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-datasets=( "classroom_s3" "davb" "tivon3" )
+#datasets=( "classroom_s3" "davb" "tivon3" )
+#lfdatapath="/home/rtwomey/code/lightfield/data/shoots"
 
-lfdatapath="/home/rtwomey/code/lightfield/data/shoots"
+datasets=( "20170725110712" "20170725112717")
+lfdatapath="/home/rtwomey/lightfield/data/houseshoots"
 
-export PATH=/usr/local/cuda-7.0/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-7.0/lib64:$LD_LIBRARY_PATH
+export PATH=/usr/local/cuda-8.0/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
 
 for ds in "${datasets[@]}"
 do
 	echo "==== processing $ds ====="
-	python undistort.py picam_calib.xml $lfdatapath/$ds/undistorted $lfdatapath/$ds/original/*.jpg
-	mkdir $lfdatapath/$ds/results
-	~/code/vsfm/vsfm/bin/VisualSFM sfm+pmvs+shared+sort+k=2514.80291008,1293.28999298,2519.66791867,928.507686634 $lfdatapath/$ds/undistorted/ $lfdatapath/$ds/results/results.nvm	
+	# mkdir $lfdatapath/$ds/results
+	python undistort.py picam_calib.xml $lfdatapath/$ds/undistorted $lfdatapath/$ds/rotated/*.jpg
+	~/code/vsfm/vsfm/bin/VisualSFM sfm+pmvs+shared+sort+pairs+k=2514.80291008,1293.28999298,2519.66791867,928.507686634 $lfdatapath/$ds/undistorted/ $lfdatapath/$ds/results/results.nvm @10	
+	# ~/code/vsfm/vsfm/bin/VisualSFM sfm+pmvs+shared+sort+k=2514.80291008,1293.28999298,2519.66791867,928.507686634 $lfdatapath/$ds/original/ $lfdatapath/$ds/results/results.nvm	
+	# ~/code/vsfm/vsfm/bin/VisualSFM sfm+pmvs+shared+sort+k=2514.80291008,1293.28999298,2519.66791867,928.507686634 $lfdatapath/$ds/rotated/ $lfdatapath/$ds/results/results.nvm @5	
+	# ~/code/vsfm/vsfm/bin/VisualSFM sfm+pmvs+shared+pairs+sort+k=2514.80291008,1293.28999298,2519.66791867,928.507686634 $lfdatapath/$ds/rotated/ $lfdatapath/$ds/results/results.nvm @5	
+	# ~/code/vsfm/vsfm/bin/VisualSFM sfm+pmvs+shared+pairs+sort $lfdatapath/$ds/rotated/ $lfdatapath/$ds/results/results.nvm @5	
+
 done
 
 
